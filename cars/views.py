baabd0677ef6fccd64660df00a98019f4c7d7df9
Farah AlOmar <farahjalomar@gmail.com>
2019-08-27 11:48:37 +0300

    first commit

--- cars/views.py
+++ cars/views.py
@@ -1,32 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
 
 def car_list(request):
-	cars = Car.objects.all()
-	context = {
-		"cars": cars,
-	}
-	return render(request, 'car_list.html', context)
+    context = {
+        "car": Car.objects.all()
+    }
+    return render(request, 'car_list.html', context)
 
 
 def car_detail(request, car_id):
-	car = Car.objects.get(id=car_id)
-	context = {
-		"car": car,
-	}
-	return render(request, 'car_detail.html', context)
+    context = {
+        "car": Car.objects.get(id=car_id)
+    }
+    return render(request, 'car_detail.html', context)
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES)
+		if form.is_valid():
+			car = form.save()
+			return redirect(car)
+	context = {
+	"form": form,
+	}
+	return render(request, 'create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance=car_obj)
+	if request.method == "POST":
+		form = CarForm(request.POST, instance=car_obj)
+		if form.is_valid():
+			form.save()
+			return redirect(car_obj)
+	context = {
+		"car_obj": car_obj,
+		"form": form,
+	}
+	return render(request, 'update.html', context)
+
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	car_obj.delete()
+	return redirect('car-list')
